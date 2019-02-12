8cabc6553717a0451dbd3bd28294da9b1b9a2935
BaronKiko <andy.075109@hotmail.co.uk>
2019-02-12 00:27:28 +0000

    Cleanup Implemented CALL

--- Chup8.cs
+++ Chup8.cs
@@ -11,18 +11,21 @@ namespace Chup_8
     {
         Random rnd = new Random();
 
-        private ushort PC;
-        private byte[] Memory;
-        private byte[] V;
-        private ushort I;
-        private ushort SP;
+        private ushort   PC;
+        private byte[]   Memory;
+        private byte[]   V;
+        private ushort   I;
+        private ushort   StackPointer;
+        private ushort[] Stack;
 
         public Chup8(string FilePath)
         {
             // Init variables
-            PC     = 0x200;
-            Memory = new byte[4096];
-            V      = new byte[16];
+            PC           = 0x200;
+            Memory       = new byte[4096];
+            V            = new byte[16];
+            StackPointer = 0;
+            Stack        = new ushort[16];
 
             //Load the ROM
             byte[] FileData = File.ReadAllBytes(FilePath);
@@ -39,6 +42,18 @@ namespace Chup_8
         }
 
 
+        private void Push(ushort value)
+        {
+            Stack[StackPointer++] = value;
+        }
+
+
+        public ushort Pop()
+        {
+            return Stack[StackPointer--];
+        }
+
+
         private void Step()
         {
             ushort instruction = (ushort)(Memory[PC] << 8 | Memory[PC + 1]);
@@ -46,34 +61,40 @@ namespace Chup_8
             // Complete instructions
             switch ((OpCode)instruction)
             {
-                
+                case OpCode.Clear:
+                    //TODO: Clear graphics
+                    PC += 2;
+                    break;
             }
 
             // Opcodes
             switch ((OpCode)(instruction & 0xF000))
             {
-                case OpCode.Annn:
-                    I = (ushort)(instruction & 0x0FFF);
-                    PC += 2;
-                    Console.WriteLine("Executed Annn");
+                // 1nnn
+                case OpCode.JP:
+                    PC = (ushort)(instruction & 0xFFF);
                     break;
 
+                // 2nnn
+                case OpCode.CALL:
+                    Push((ushort)(PC + 2));
+                    PC = (ushort)(instruction & 0xFFF);
+                    break;
+
+                //Cxkk
                 case OpCode.RND:
                     V[(instruction & 0x0F00) >> 8] = (byte)(rnd.Next() & instruction);
                     PC += 2;
-                    Console.WriteLine("Executed RND");
                     break;
 
-                case OpCode.JP:
-                    PC = (byte)(instruction & 0xFFF);
-                    break;
-
-                case OpCode.Clear:
-                    //TODO: Clear graphics
+                // Annn
+                case OpCode.LDI:
+                    I = (ushort)(instruction & 0x0FFF);
                     PC += 2;
                     break;
 
-                default: throw new Exception($"Instruction [0x{instruction:X4}] is unknown!");
+                default:
+                    throw new Exception($"Instruction [0x{instruction:X4}] is unknown!");
             }
         }
     }
--- OpCode.cs
+++ OpCode.cs
@@ -4,7 +4,8 @@
     {
         Clear = 0x00E0,
         JP    = 0x1000,
-        Annn  = 0xA000,
+        CALL  = 0x2000,
+        LDI   = 0xA000,
         RND   = 0xC000,
     }
 }
